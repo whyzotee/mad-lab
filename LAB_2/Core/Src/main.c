771cba442aab23bad99ec126b52cc696b5c451cb
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2025-12-30 01:24:01 +0700

    feat: Lab 2 remake ver. play with register

--- LAB_2/Core/Src/main.c
+++ LAB_2/Core/Src/main.c
@@ -42,10 +42,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-uint8_t num = 0;
-uint8_t led_auto = 0;
-uint8_t pin[] = { GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_2, GPIO_PIN_3, GPIO_PIN_4,
-		GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7 };
+uint8_t n=0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -94,7 +91,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-//  uint8_t n;
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -104,76 +101,44 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-//	if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == 1) {
-//		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, 1);
-//
-//		HAL_Delay(1000);
-//
-//		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, 0);
-//	}
-//
-//	if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == 0) {
-//		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, 1);
-//
-//		HAL_Delay(1000);
-//
-//		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, 0);
-//	}
+	  if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == 1) {
+		  if (n >= 8)
+		  	n = 0;
 
-//	if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == 0) {
-//		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2, 1);
-//
-//		HAL_Delay(1000);
-//
-//		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2, 0);
-//	}
+		  GPIOD->BSRR = 0x00FF0000;
+		  GPIOD->BSRR = 0x00000001 << n;
 
+		  n += 1;
+	  }
 
-	if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == 1) {
-		num++;
+	  if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == 0) {
+		  uint32_t odr;
 
-		if (num>8)
-			num = 1;
+		  for (uint8_t i=0; i<=7; i++) {
+			  odr = GPIOD->ODR;
 
-		HAL_Delay(300);
-	}
+			  GPIOD->BSRR = 0x00FF0000;
 
-	if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == 0) {
-		if (num == 0)
-			led_auto = 1;
+			  if (odr == 0 || odr == 1) {
+				  GPIOD->BSRR = 0x00000080;
+			  } else if(odr > 0){
+				  GPIOD->BSRR = odr >> 1;
+			  } else {
+				  GPIOD->BSRR = 0x00000080 >> i;
 
+				  if (i == 7)
+					  GPIOD->BSRR = 0x00000001;
+			  }
 
-		for(uint8_t i=0; i < 8; i++) {
-			num--;
+			  HAL_Delay(300);
+		  }
 
-			if (num > 8 || num == 0)
-				num = 8;
+		  if (n == 0) {
+			  GPIOD->BSRR = 0x00FF0000;
+		  }
+	  }
 
-			for(uint8_t i=0; i < 8; i++) {
-				HAL_GPIO_WritePin(GPIOD, pin[i], num==i+1);
-			}
-
-			HAL_Delay(300);
-
-			if (led_auto == 1 && num == 1) {
-				num = 0;
-				led_auto = 0;
-			}
-
-		}
-	}
-
-	for(uint8_t i=0; i < 8; i++) {
-		HAL_GPIO_WritePin(GPIOD, pin[i], num==i+1);
-	}
-
-//	for (n=0; n<=2; n++) {
-//		GPIOC -> BSRR = 0x07000000;
-//		HAL_Delay(500);
-//
-//		GPIOC -> BSRR = 0x00000400 >> n;
-//		HAL_Delay(500);
-//	}
+	  HAL_Delay(100);
   }
   /* USER CODE END 3 */
 }
@@ -247,14 +212,7 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
@@ -271,20 +229,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PB0 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PC8 PC9 PC10 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PD0 PD1 PD2 PD3
                            PD4 PD5 PD6 PD7 */
